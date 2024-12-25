d6cefdcd6f558bc7ca7934d1955e4a596b9306b4
AgentKharuk <harukv@gmail.com>
2024-12-25 13:30:35 -0800

    arrowHandler [Changed] add movements on arrows and wasd

--- src/arrowHandler.js
+++ src/arrowHandler.js
@@ -1,16 +1,32 @@
 export function addArrowHandler () {
-    const snakeHead = document.querySelector('#snake-head')
+    const snakeHead = document.querySelector('#snakeHead')
+    const goDown = (element) => {
+        const topOffset = parseInt(element.style.top)
+        element.style.top = topOffset + 100 + 'px';
+    }
+    const goUp = (element) => {
+        const topOffset = parseInt(element.style.top)
+        element.style.top = topOffset - 100 + 'px';
+    }
+    const goLeft = (element) => {
+        const leftOffset = parseInt(element.style.left)
+        element.style.left = leftOffset - 100 + 'px';
+    }
+    const goRight = (element) => {
+        const leftOffset = parseInt(element.style.left)
+        element.style.left = leftOffset + 100 + 'px';
+    }
     const arrowHandler = (event) => {
         if (event.key === "ArrowUp" || event.key === "w") {
-            console.log("змея двигается вверх");   
+            goUp(snakeHead)  
         } else if (event.key === "ArrowRight" || event.key === "d") {
-            console.log("змея двигается вправо");  
+            goRight(snakeHead) 
         } else if (event.key === "ArrowDown" || event.key === "s") {
-            console.log("змея двигается вниз");  
+            goDown(snakeHead) 
         } else if (event.key === "ArrowLeft" || event.key === "a") {
-            console.log("змея двигается влево");  
+            goLeft(snakeHead) 
         }
     }
     document.addEventListener('keydown', arrowHandler) 
 }
-addArrowHandler()
+
