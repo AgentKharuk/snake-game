0b9a9d9aa51be0b578184e528d3704a549654d14
AgentKharuk <harukv@gmail.com>
2024-12-25 11:56:48 -0800

    arrowHandler [Changed] add wasd handler

--- src/arrowHandler.js
+++ src/arrowHandler.js
@@ -1,15 +1,15 @@
 export function addArrowHandler () {
     const snakeHead = document.querySelector('#snake-head')
     const arrowHandler = (event) => {
-        if (event.key === "ArrowUp") {
-            console.log(event.key);   
-        } else if (event.key === "ArrowRight") {
-            console.log(event.key);  
-        } else if (event.key === "ArrowDown") {
-            console.log(event.key);  
-        } else if (event.key === "ArrowLeft") {
-            console.log(event.key);  
-        } 
+        if (event.key === "ArrowUp" || event.key === "w") {
+            console.log("змея двигается вверх");   
+        } else if (event.key === "ArrowRight" || event.key === "d") {
+            console.log("змея двигается вправо");  
+        } else if (event.key === "ArrowDown" || event.key === "s") {
+            console.log("змея двигается вниз");  
+        } else if (event.key === "ArrowLeft" || event.key === "a") {
+            console.log("змея двигается влево");  
+        }
     }
     document.addEventListener('keydown', arrowHandler) 
 }
